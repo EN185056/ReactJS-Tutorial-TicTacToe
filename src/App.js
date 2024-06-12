54d92d574c7d018efabf741ea799563ca75b45cf
Ethan Nguyen-Tu <EN185056@ncr.com>
2024-06-12 10:21:38 -0400

    Tutorial(State): Removed redundant state saving of xIsNext

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ export function Board({xIsNext, squares, onPlay}) {
 
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
 
@@ -61,12 +61,10 @@ export default function Game() {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0)
   }
 
   const moves = history.map((squares, move) => {
