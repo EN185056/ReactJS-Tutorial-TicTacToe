0e4669fd9457486a807beba46912203b804e44d8
Ethan Nguyen-Tu <EN185056@ncr.com>
2024-06-12 10:30:31 -0400

    Bug(Semicolon): Removed semicolons to the right of the Tic-Tac-Toe board

--- src/App.js
+++ src/App.js
@@ -31,19 +31,19 @@ export function Board({xIsNext, squares, onPlay}) {
     <>
       <div className='status'>{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />;
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />;
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />;
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />;
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />;
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />;
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />;
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />;
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />;
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
